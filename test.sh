c9b54507f02e896e6d9310cbe0a948aaf0bb014d
David Souther <davidsouther@gmail.com>
2015-01-10 09:54:37 -0500

    Reworked the test linking.

--- test.sh
+++ test.sh
@@ -5,11 +5,12 @@ EXIT=$?
 
 EXAMPLES="vanilla angular bare"
 
-npm ln
 for F in $EXAMPLES ; do
   cd examples/$F
   echo "Running tests in $(pwd)"
-  npm i ; npm ln rupert-grunt # Use local rupert-grunt
+  npm i
+  rm -rf node_modules/rupert-grunt/src
+  cp -r ../../src ./node_modules/rupert-grunt
   npm test ; ERR=$?
   if [ $ERR -ne 0 ] ; then
     EXIT=$ERR
